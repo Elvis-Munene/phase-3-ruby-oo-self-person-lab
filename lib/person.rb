15100da6684b143c4533a6221714d7979c9afecb
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-08-20 10:23:03 +0300

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,56 @@
 # your code goes here
+class Person
+ attr_reader :name
+ attr_accessor :bank_account, :happiness, :hygiene
+
+ def initialize(name)
+    @name = name
+    @happiness= 8
+    @hygiene= 8
+ end
+
+ def bank_account
+    @bank_account
+ end
+
+
+ def bank_account(bank_account= 25)
+    @bank_account = bank_account
+ end
+
+def happiness
+    @happiness
+end
+
+def happiness=(num)
+   @happiness= if num > 10 
+        @happiness = 10
+    
+    elsif num < 0 
+        @happiness = 0
+
+    else 
+        @happiness = num
+    end
+end
+
+
+def hygiene
+    @hygiene
+end
+
+def hygiene=(num)
+    
+  @hygiene= if num > 10 
+        @hygiene = 10
+    
+    elsif num < 0 
+        @hygiene = 0
+
+    else 
+        @hygiene = num
+    end
+end
+end
+
+
